8211ad41655c92ea4a1251394dd430f6bfdba796
Sayali Gonjari <gonjari.sayali96@gmail.com>
2020-08-25 12:49:48 +0530

    initial commit

---
+++ src/First.kt
@@ -0,0 +1,67 @@
+
+fun main(args:Array<String>){
+
+
+    var i = 10
+    var j= 10
+
+    val sum :Int
+    i+=j
+
+
+
+
+
+
+    /***********************
+    var i :Long = 8535465223456
+    println(i.toInt())
+    println(i.toByte())
+    println(i.toDouble())
+    //println(i.toLong())
+    println(i.toShort())
+    println(i.toString())
+    println(i.toFloat())****************/
+
+   /**** val i='A'
+    val j:Char='P'
+    println(i)
+    println(j)**********/
+
+
+    /******************
+    val i : Int = 89464665
+    val j="Kotlin"
+
+    print("Value of i is $i \nj is $j")*/
+
+
+
+
+/********************************
+var i:String ="John Watson"
+    i="William"
+    print("String Value is $i")*****************/
+
+/*************************************
+    var i:Int =10
+    var j:Int = 20
+
+    val sum = (i+j)
+    val sub = i-j
+    val mul = i*j
+    val div = i/j
+    val modVal = i%j
+    println("$sum")
+    println("$sub")
+    println("$mul")
+    println("$div")
+    println("$modVal")
+*************************************/
+
+    /********************************/
+  /* val i :Int= 78
+    //=98
+    print("$i")*/
+
+}
